d634a0caff8607157e220bebc95bc075837565d3
Lukasz Zajaczkowski <lukasz.zajaczkowski@ts.fujitsu.com>
2017-04-18 07:23:41 +0200

    volumes added

--- installation/install.sh
+++ installation/install.sh
@@ -6,6 +6,6 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.18.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.16.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 
 
